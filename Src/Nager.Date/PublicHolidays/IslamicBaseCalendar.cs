71e7908d82e84d8b43c5cda97e9ff2775bdb5a93
rubenmonteiro <ruben.monteiro@gmail.com>
2018-03-06 18:58:32 +0000

    Islamic base calendar

---
+++ Src/Nager.Date/PublicHolidays/IslamicBaseCalendar.cs
@@ -0,0 +1,22 @@
+﻿using Nager.Date.Contract;
+using Nager.Date.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nager.Date.PublicHolidays
+{
+    public abstract class IslamicBaseCalendar : ICountryCalendarProvider
+    {
+        public virtual DayOfWeek FirstDayOfWeek => DayOfWeek.Sunday;
+
+        public abstract IEnumerable<PublicHoliday> Get(int year);
+
+        public virtual bool IsWeekend(DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Friday || date.DayOfWeek == DayOfWeek.Saturday;
+        }
+    }
+}
